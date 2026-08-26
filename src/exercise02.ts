da086b0144b15550a80bfd9dd65a46604fc8f78d
Rbarsi <rbarsi@charlotte.edu>
2026-08-26 17:00:57 -0400

    Completed exercise 2

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,34 @@
+export class InvalidNucleotide extends Error{
+  constructor(nucleotide: string) {
+    super(`Invalid nucleotide: ${nucleotide}`);
+    this.name = "InvalidNucleotide";
+  }
+}
+
 export function transcribeDNA(dna: string): string {
-  return '';
+  let rna = "";
+
+  for (const nucleotide of dna) {
+    switch (nucleotide) {
+      case "A":
+        rna += "U";
+        break;
+
+      case "T":
+        rna += "A";
+        break;
+
+      case "C":
+        rna += "G";
+        break;
+
+      case "G":
+        rna += "C";
+        break;
+
+      default:
+        throw new InvalidNucleotide(nucleotide);
+    }
+  }
+  return rna;
 }
