4d59a29f343c389af557bc0818da63930dfb04e0
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 09:50:01 -0400

    exercise 10 complete

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,44 @@
-type UserAccount = {};
+export type UserAccount = {
+  id: string;
+  createdAt: Date;
+  email: string;
+  passwordHash: string;
+  profile: {
+    bio: string;
+    avatarUrl: string;
+  };
+};
 
 export class UserRegistry {
+  private users: UserAccount[] = [];
+
   public registerUser(
-    data: Omit<UserAccount, 'id' | 'createdAt'>,
+    data: Omit<UserAccount, "id" | "createdAt">,
   ): UserAccount {
-    return {};
+    const user: UserAccount = {
+      ...data,
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
   }
 
   public getUserView(
     id: string,
-  ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+  ): Readonly<Pick<UserAccount, "id" | "email" | "profile">> | undefined {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      return undefined;
+    }
+
+    return Object.freeze({
+      id: user.id,
+      email: user.email,
+      profile: user.profile,
+    });
   }
 }
