eae6b0af1b966d57117b7bad6a9d941a267e8236
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 10:00:40 -0400

    exercise 11 complete

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,11 @@
+import { promises as fs } from "fs";
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timestamp = new Date().toISOString();
+  const logMessage = `${statusMessage} ${timestamp}\n`;
+
+  await fs.appendFile(filePath, logMessage, "utf-8");
+}
