b7a573fc472d609bbe91296f86e72579fbc4987b
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 09:27:03 -0400

    exercise 8 complete

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,26 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private handlers: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.handlers[eventName]) {
+      this.handlers[eventName] = [];
+    }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+    this.handlers[eventName]!.push(callback);
+  }
+
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const callbacks = this.handlers[eventName];
+
+    if (callbacks) {
+      callbacks.forEach((callback) => callback(data));
+    }
+  }
 }
