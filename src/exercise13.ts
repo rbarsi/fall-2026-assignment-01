e630dbde63e2add25ca349a1a03ebfbb877d7a7e
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 10:07:03 -0400

    exercise 13 complete

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,26 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
-export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+export async function fetchTodoSafe(
+  todoId: number,
+): Promise<TodoItem | null> {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    );
+
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch todo");
+    }
+
+    const todo = (await response.json()) as TodoItem;
+
+    return todo;
+  } catch {
+    return null;
+  }
 }
