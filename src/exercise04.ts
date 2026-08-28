462135bf2c2d3c4974624b5b586f35e119910e2b
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 08:48:59 -0400

    Completed exercise 4 with passed test

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,29 @@
-export type Circle = {};
+export type Circle = {
+  kind: "circle";
+  radius: number;
+};
 
-export type Rectangle = {};
+export type Rectangle = {
+  kind: "rectangle";
+  width: number;
+  height: number;
+};
 
-export type Square = {};
+export type Square = {
+  kind: "square";
+  sideLength: number;
+};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
-  return 0;
+  if (shape.kind === "circle"){
+    return Math.PI * shape.radius * shape.radius;
+  }
+
+  if (shape.kind === "rectangle"){
+    return shape.width * shape.height;
+  }
+
+  return shape.sideLength * shape.sideLength;
 }
