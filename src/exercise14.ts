c40d2c75bda554ec9e91bb83248d352c00df26b8
Rbarsi <rbarsi@charlotte.edu>
2026-08-28 10:08:44 -0400

    exercise 14 complete

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,21 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
-export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+export async function fetchPostBatch(
+  postIds: number[],
+): Promise<PostItem[]> {
+  const requests = postIds.map(async (id) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+
+    const post = (await response.json()) as PostItem;
+
+    return post;
+  });
+
+  return Promise.all(requests);
 }
